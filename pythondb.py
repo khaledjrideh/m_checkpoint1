4eb11f01930c5e0a7aa12d128edd1fe3e658b96b
Khaled Jrideh <khaled.jrideh@brightstraining.com>
2024-07-02 15:36:03 +0200

    complete the code

--- pythondb.py
+++ pythondb.py
@@ -1,8 +1,19 @@
 import psycopg2
 
+hostname = 'localhost'
+database = 'dvdrental'
+username = 'postgres'
+pwd = 'Beornottobe1'
+port_id = 5432
+
 def db_connection():
     return psycopg2.connect(
-        host="localhost"
+        host="localhost",
+        dbname = database,
+         user = username,
+        password = pwd,
+        port = port_id
+
 )
 
 def read_dict():
@@ -19,6 +30,9 @@ while True: ## REPL - Read Execute Program Loop
     cmd = input("Command: ")
 
     if cmd == "list":
+        rows = read_dict()
+        for row in rows:
+            print(f"id: {row[0]}, word: {row[1]}, translation: {row[2]}")
         
     elif cmd == "quit":
         exit()
